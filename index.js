e071c2711d5cb06b9b44cde3a8873a5d3fe9b836
Elyse Wells <cewells89@gmail.com>
2020-10-27 20:20:28 -0400

    added styling to hide scrollspy on mobile

--- index.js
+++ index.js
@@ -3,7 +3,3 @@ $('body').scrollspy({ target: '#navPane' })
 $('[data-spy="scroll"]').each(function () {
     var $spy = $(this).scrollspy('refresh')
   });
-
-  $('[data-spy="scroll"]').on('activate.bs.scrollspy', function () {
-    // do something…
-  });
